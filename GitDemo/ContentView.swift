9940327068ec56c4bbace5a9526bcb995af9e0ce
Alon Ravid <alonri75@walla.com>
2021-08-09 14:17:51 +0300

    Initial Commit

---
+++ GitDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitDemo
+//
+//  Created by Alon Ribovsky on 09/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
