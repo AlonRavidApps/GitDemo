514e85983edd51e522f60aa546a84ef9fb437f3a
Alon Ravid <alonri75@walla.com>
2021-08-09 14:24:21 +0300

    Commit - for GitHub

--- GitDemo/ContentView.swift
+++ GitDemo/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world! - branch alon")
+        Text("Hello, world! - branch alon Github")
             .padding()
     }
 }
